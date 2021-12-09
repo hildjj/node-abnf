e214501de3d1c5d6659a5f1380ce7177e0012413
Joe Hildebrand <joe-github@cursive.net>
2021-12-09 12:27:36 -0700

    Working CLI

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,16 @@
 module.exports = {
   root: true,
   extends: "@peggyjs",
-  ignorePatterns: [
-  ],
-  overrides: [
-    {
-      files: ["*.mjs"],
-      parserOptions: {
-        sourceType: "module",
-        ecmaVersion: 2018,
-      },
-      rules: {
-        "comma-dangle": ["error", {
-          arrays: "always-multiline",
-          objects: "always-multiline",
-          imports: "always-multiline",
-          exports: "always-multiline",
-          functions: "never",
-        }],
-      },
-    },
-  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "comma-dangle": ["error", {
+      arrays: "always-multiline",
+      objects: "always-multiline",
+      imports: "always-multiline",
+      exports: "always-multiline",
+      functions: "never",
+    }],
+  },
 };
