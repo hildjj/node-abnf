d4fa4734d048fed988718cf24509313686fb9337
Joe Hildebrand <joe-github@cursive.net>
2024-04-19 09:17:46 -0600

    Add type checking for base class inputs, to prevent location elision in the future

--- test/ast.ava.js
+++ test/ast.ava.js
@@ -6,3 +6,8 @@ test("range escape", t => {
   t.is(ast.Range.escape(0x7c), "\\x7c");
   t.is(ast.Range.escape(0x100), "\\u0100");
 });
+
+test("bad base class types", t => {
+  t.throws(() => new ast.Base());
+  t.throws(() => new ast.Range("", 0, 1));
+});
